27fe6c7a8d87f40f1f887142dcd6c619c71712e7
Sudhanshu-jena <63591527+Sudhanshu-jena@users.noreply.github.com>
2021-06-01 23:10:52 +0530

    Update

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
+
 import django_heroku
 from pathlib import Path
 
@@ -121,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = BASE_DIR
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
